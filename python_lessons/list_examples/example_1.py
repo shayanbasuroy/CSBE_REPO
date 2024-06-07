2e65aa61fa9c1f74c960ccb03bab945bfe231adb
shayanbasuroy <103628425+shayanbasuroy@users.noreply.github.com>
2024-06-07 09:55:22 +0530

    added the examples of list

---
+++ python_lessons/list_examples/example_1.py
@@ -0,0 +1,21 @@
+"""
+This is my first list example
+"""
+
+list_1 = [1, 2, 3, 4, 5]
+print(list_1)
+print(len(list_1))
+print((list_1[0]))
+print((list_1[1]))
+print((list_1)[2])
+print((list_1)[3])
+print((list_1)[4])
+
+"""
+how to format
+"""
+print(f"The first value of the list is {(list_1[0])}")
+print((list_1[1]))
+print((list_1)[2])
+print((list_1)[3])
+print((list_1)[4])
--- ref-1.md
+++ ref-1.md
@@ -5,3 +5,24 @@
 # cd /Users/shayanbasuroy/Document/CBSE
 
 # CBSE code .
+
+# git add .
+
+# git commit -m "first commit"
+
+# git branch -M main
+
+# git remote add origin https://github.com/shayanbasuroy/CSBE_REPO.git
+
+# git push -u origin main
+
+# --------
+
+# it checkout  -b list_examples
+
+
+
+
+
+
+
